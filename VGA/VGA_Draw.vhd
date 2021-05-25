95c123ba426d1575d15540d539157a2ed8f73ebe
Yves Savoy <10594588+yvessavoy@users.noreply.github.com>
2021-05-25 19:35:20 +0200

    Formatting fixed in VGA_Draw

--- VGA/VGA_Draw.vhd
+++ VGA/VGA_Draw.vhd
@@ -15,17 +15,15 @@ use work.Snake_Globals.all;
 --------------------------------------------------------------------
 entity VGA_Draw is
 	port (
-		CLK			: in std_logic;
-		Reset			: in std_logic;
-		Drawable 	: in std_logic;
-		X_Pos			: in integer range 0 to 799;
-		Y_Pos			: in integer range 0 to 524;
-		R				: out std_logic_vector(3 downto 0);
-		G				: out std_logic_vector(3 downto 0);
-		B				: out std_logic_vector(3 downto 0);
-		Snake			: in Snake_t;
-		Food			: in Point2D_t;
-		Text_On		: in std_logic								-- 1 = Set current pixel for text to be drawn
+		CLK		: in std_logic;
+		Reset		: in std_logic;
+		Drawable        : in std_logic;
+		X_Pos 		: in integer range 0 to 799;
+		Y_Pos		: in integer range 0 to 524;
+		R		: out std_logic_vector(3 downto 0);
+		G		: out std_logic_vector(3 downto 0);
+		B		: out std_logic_vector(3 downto 0);
+		Text_On		: in std_logic
 	);
 end VGA_Draw;
 
@@ -43,24 +41,7 @@ begin
 			B <= x"0";
 			
 			if Drawable = '1' then
-				for i in 0 to Max_Snake_Length loop
-					-- Only draw squares within the snakes total length
-					if i < Snake.Length then
-						if X_Pos >= Snake.Positions(i).X and X_Pos < Snake.Positions(i).X + Square_Width and Y_Pos >= Snake.Positions(i).Y and Y_Pos < Snake.Positions(i).Y + Square_Height then
-							R <= x"F";
-						end if;
-					end if;
-				end loop;
-				
-				-- There is always a food on the screen, so no need
-				-- to check if it needs to be drawn or not
-				if X_Pos >= Food.X and X_Pos < Food.X + Square_Width and Y_Pos >= Food.Y and Y_Pos < Food.Y + Square_Height then
-					G <= x"F";
-				end if;
-				
-				if Text_On = '1' then
-					B <= x"F";
-				end if;
+				-- Implement drawing of RGB here
 			end if;
 			
 			if Reset = '1' then
